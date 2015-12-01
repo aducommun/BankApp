b97cd304e6555a29a4c654052ebe95fdebf03cd3
aducommun <alexandr.ducommun@EGE-DUCOMMUNA.intra.eiaj.ch>
2015-12-01 17:31:54 +0100

    CustomersBean enfin fonctionnel..

---
+++ src/java/ch/hearc/ig/odi/customeraccount/bean/CustomersBean.java
@@ -0,0 +1,26 @@
+package ch.hearc.ig.odi.customeraccount.bean;
+
+import ch.hearc.ig.odi.customeraccount.business.Customer;
+import ch.hearc.ig.odi.customeraccount.services.Services;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author alexandr.ducommun
+ */
+
+@Named (value="customerBean")
+@SessionScoped
+public class CustomersBean implements Serializable {
+    
+    @Inject Services services;
+    
+    @Inject
+    public List<Customer> getCustomers() {
+        return services.getCustomersList();
+    }
+}
