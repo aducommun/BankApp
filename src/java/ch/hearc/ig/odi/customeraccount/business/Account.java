50c7221a04f46e5b8db38631edb91f0db6bd8283
aducommun <alexandr.ducommun@EGE-DUCOMMUNA.intra.eiaj.ch>
2015-12-03 08:30:16 +0100

    Correction des méthodes debit/credit

--- src/java/ch/hearc/ig/odi/customeraccount/business/Account.java
+++ src/java/ch/hearc/ig/odi/customeraccount/business/Account.java
@@ -2,96 +2,104 @@ package ch.hearc.ig.odi.customeraccount.business;
 
 public class Account {
 
-	private Customer customer;
-	private String number;
-	private String name;
-	private double balance = 0;
-	private double rate = 0.001;
+    private String number;
+    private String name;
+    private double balance = 0;
+    private double rate = 0.001;
+    private Customer customer;
 
-	public String getName() {
-		return this.name;
-	}
+    /**
+     *
+     * @param number Account number
+     * @param name Account name
+     * @param rate Account rate
+     * @param customer Account owner
+     */
+    public Account(String number, String name, double rate, Customer customer) {
+        this.number = number;
+        this.name = name;
+        this.rate = rate;
+        this.customer = customer;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getBalance() {
-		return this.balance;
-	}
-
-	public void setBalance(double balance) {
-		this.balance = balance;
-	}
-
-	public double getRate() {
-		return this.rate;
-	}
-
-	public void setRate(double rate) {
-		this.rate = rate;
-	}
-
-        public Customer getCustomer() {
-            return customer;
+    /**
+     *
+     * @param amount Credit amount
+     */
+    public void credit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        } else {
+            throw new IllegalStateException("Error, amount doit être plus grand que 0.");
         }
+    }
 
-        public void setCustomer(Customer customer) {
-            this.customer = customer;
+    /**
+     *
+     * @param amount Debit amount
+     */
+    public void debit(double amount) {
+        if (this.balance - amount < 0) {
+            throw new IllegalStateException("Solde insuffisant pour la transaction");
+        } else if (amount < 0) {
+            throw new IllegalStateException("Error, amount doit être plus grand que 0.");
+        }else {
+            this.balance -= amount;
         }
+    }
 
-        public String getNumber() {
-            return number;
+    /**
+     *
+     * @param amount Amount for transfer
+     * @param source Source account for transfer
+     * @param target Target account for transfer
+     */
+    public static void transfer(double amount, Account source, Account target) {
+        if (amount <= 0) {
+            throw new IllegalStateException("amount doit être plus grand que 0.");
         }
+        source.debit(amount);
+        target.credit(amount);
+    }
 
-        public void setNumber(String number) {
-            this.number = number;
-        }
-        
-        
+    public String getName() {
+        return this.name;
+    }
 
-	/**
-	 * 
-	 * @param number Account number
-	 * @param name Account name
-	 * @param rate Account rate
-	 * @param customer Account owner
-	 */
-	public Account(String number, String name, double rate, Customer customer) {
-		this.number = number;
-                this.name = name;
-                this.rate = rate;
-                this.customer = customer;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * 
-	 * @param amount Credit amount
-	 */
-	public void credit(double amount) {
-		setBalance(getBalance() + amount);
-	}
+    public double getBalance() {
+        return this.balance;
+    }
 
-	/**
-	 * 
-	 * @param amount Debit amount
-	 */
-	public void debit(double amount) {
-		setBalance(getBalance() - amount);
-	}
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 
-	/**
-	 * 
-	 * @param amount Amount for transfer
-	 * @param source Source account for transfer
-	 * @param target Target account for transfer
-	 */
-	public static void transfer(double amount, Account source, Account target) {
-		if ((source.getBalance() - amount) < 0) {
-                    throw new IllegalStateException("Solde insuffisant");
-                }
-                source.debit(amount);
-                target.credit(amount);
-	}
+    public double getRate() {
+        return this.rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 
 }
